c40b58bfa7a90f7fa9168cdac9e8870b5af607ff
Matthew Brookes <matthew.brookes15@imperial.ac.uk>
2016-02-20 13:24:49 +0000

    Create an AsyncTask for downloading a string
    This class downloads a string from a URL

---
+++ app/src/main/java/com/alpaca/app/DownloadString.java
@@ -0,0 +1,34 @@
+package com.alpaca.app;
+
+import android.os.AsyncTask;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+public class DownloadString extends AsyncTask<String, Void, String> {
+    @Override
+    protected String doInBackground(String... params) {
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpContext localContext = new BasicHttpContext();
+        HttpGet httpGet = new HttpGet(params[0]);
+        try {
+            HttpResponse response = httpClient.execute(httpGet, localContext);
+            HttpEntity entity = response.getEntity();
+            return EntityUtils.toString(entity);
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}

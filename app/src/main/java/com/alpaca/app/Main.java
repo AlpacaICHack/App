89624cb9c659d6a4c330aea2e72f6944851be0fc
Martin Zlocha <zlocha.martin@gmail.com>
2016-02-20 16:55:59 +0000

    Changed which layout file should be used.

--- app/src/main/java/com/alpaca/app/Main.java
+++ app/src/main/java/com/alpaca/app/Main.java
@@ -10,7 +10,7 @@ public class Main extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_main);
 
         Intent extras = getIntent();
         int eventID;

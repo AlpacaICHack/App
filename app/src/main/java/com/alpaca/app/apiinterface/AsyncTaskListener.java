c60e46cd53a086a848080722440d32d416c7a31a
Matthew Brookes <matthew.brookes15@imperial.ac.uk>
2016-02-20 14:06:25 +0000

    Implement callback when downloading string

---
+++ app/src/main/java/com/alpaca/app/apiinterface/AsyncTaskListener.java
@@ -0,0 +1,5 @@
+package com.alpaca.app.apiinterface;
+
+public interface AsyncTaskListener {
+    void onStringDownloaded(String str);
+}
--- app/src/main/java/com/alpaca/app/apiinterface/DownloadString.java
+++ app/src/main/java/com/alpaca/app/apiinterface/DownloadString.java
@@ -15,6 +15,14 @@ import org.apache.http.util.EntityUtils;
 import java.io.IOException;
 
 public class DownloadString extends AsyncTask<String, Void, String> {
+
+    // Callback allows the string to be passed to another class
+    private AsyncTaskListener callback;
+
+    public DownloadString(AsyncTaskListener caller){
+        callback = caller;
+    }
+
     @Override
     protected String doInBackground(String... params) {
         HttpClient httpClient = new DefaultHttpClient();
@@ -30,5 +38,10 @@ public class DownloadString extends AsyncTask<String, Void, String> {
             e.printStackTrace();
         }
         return null;
+    }
+
+    @Override
+    protected void onPostExecute(String str){
+        callback.onStringDownloaded(str);
     }
 }
